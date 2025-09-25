350397e1d0a1e72c5deb2f698d0942a6dc6b5440
Rini Grace Beera <gracebeera2000@gmail.com>
2025-09-25 17:22:22 +0530

    Added server.js and book model

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const cors = require("cors")
+
+const dbConfig = require("./src/config/db.config");
+
+const app = express();
+
+var corsOptions={
+    origin:"http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}))
+
+const db = require("./src/models");
+const Books = db.Books
+
+db.mongoose.
+connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log("Sucessfully connected to Mongo db")
+})
+.catch(err =>{
+    console.error("connection error",err);
+    process.exit()
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+//global error handler
+app.use((error,request,response,next)=>{
+    console.error(error.stack);
+    response.status(error.status || 500).json({error:'Internal server error'})
+})
---
+++ src/models/book.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Book = mongoose.model(
+    "Book",
+    new mongoose.Schema({
+        title:String,
+        author:String,
+        genre:String,
+        year:Number,
+        description:String,
+    })
+);
+
+module.exports = Book;
